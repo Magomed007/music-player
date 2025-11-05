b2f10aaf88c7c72b88100d1922952939997ba7b9
Magomed <mister-maga02@mail.ru>
2025-11-05 16:47:01 +0300

    Delete scale.js

--- scale.js
+++
@@ -1,6 +0,0 @@
-function setScale() {
-    const scale = window.innerWidth / 1440;
-    document.querySelector('meta[name="viewport"]')
-        .setAttribute('content', `width=1440, initial-scale=${scale}, user-scalable=yes`);
-}
-window.addEventListener('load', setScale);
